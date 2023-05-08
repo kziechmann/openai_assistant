ceee9982e131d06014ad88ab77d7908405fa47d9
Kevin Ziechmann <kziechmann@gmail.com>
2023-05-07 20:03:05 -0700

    fix prompt as input to script

--- openai_assistant_script.py
+++ openai_assistant_script.py
@@ -1,21 +1,22 @@
 import os
 import openai
-# from dotenv import load_dotenv
-# load_dotenv('/my_openai_apikey.env')
-# my_api_key = os.getenv('my_api_key')
-
-openai.api_key = "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"
+from dotenv import load_dotenv
+load_dotenv('/my_openai_apikey.env')
+my_api_key = os.getenv('my_api_key')
 
 message = {
     "role":"user",
-    "content": input("Good morning!\n\n Let's start your day off right by organizing your priorities. \nPlease tell me your top three tasks for today, and I'll help you schedule them.\n You can also specify any deadlines or time constraints.\n Once we have a plan, I can send you reminders via Twilio or add the tasks to your Google Calendar.\n Let's get started! \n\nTo exit say 'done!!!' \n Priorities:")
-};
+    "content": input("\nGood morning! Let's start your day off right by organizing your priorities. \n\nPlease tell me your top three tasks for today, and I'll help you schedule them.\n\nYou can also specify any deadlines or time constraints.\nOnce we have a plan, I can send you reminders via Twilio or add the tasks to your Google Calendar.\n\nLet's get started! \n\n   To exit say 'done!!!' \n Priorities:")
+}
 
+prompt = "Prompt user, listen and store input, extract ideas, priorities, and goals, prompt for clarifications if needed, provide feedback and suggestions using algorithms, offer tools and resources, use sentiment analysis and NLG for motivational feedback, thank user and prompt for additional needs, repeat steps 2-8 as needed to help user achieve goals and improve productivity."
 conversation = [{
     "role": "system",
-     "content": "DIRECTIVE_FOR_gpt-3.5-turbo",
-     "prompt": "Prompt user, listen and store input, extract ideas, priorities, and goals, prompt for clarifications if needed, provide feedback and suggestions using algorithms, offer tools and resources, use sentiment analysis and NLG for motivational feedback, thank user and prompt for additional needs, repeat steps 2-8 as needed to help user achieve goals and improve productivity.",
-}]
+     "content": "DIRECTIVE_FOR_gpt-3.5-turbo"
+},
+{"role": "system", "content": prompt}
+]
+
 
 while(message["content"]!="done!!!"):
     conversation.append(message)
